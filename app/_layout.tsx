1770796710cd0838dcb0e3067e47284cc7e1c206
Mehta Dhruv Ketan <146522630+dhruv0485@users.noreply.github.com>
2025-07-22 19:09:16 +0530

    Updated package-lock.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,11 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <GestureHandlerRootView style={styles.container}>
       <SafeAreaProvider>
